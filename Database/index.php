2b231249b1f8d2d8f1071aaa6258531701a10c77
Md.Masud karim <33942219+Masud2017@users.noreply.github.com>
2019-03-14 21:27:05 +0600

    Update index.php

--- Database/index.php
+++ Database/index.php
@@ -1,3 +1,4 @@
+<!--This docuement is a quoted /-->
 <!DOCTYPE html>
 <html>
 <head lang = "EN-US">
@@ -5,32 +6,51 @@
 <link rel = "stylesheet" type = "text/css" href = "style.css">
 <meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
 <meta name = "charset" content = "utf-8">
+<meta name = "Keywords" content = "Masud karim, Tehctrickpedia, a003304256, how techtrickpedia works, Maxpower database, Maxpower | Database">
+<meta name = "robots" content = "index,follow">
+<meta name = "Dc.title" content = "Maxpower | Database>
+<meta name = "Developer" content = "Masud karim">
+<meta name = "Description" content = "A self-tought ethical hacker">
+
 <script>
 if($_SERVER["REQUEST_METHOD"] == '0')
 {
 document.getElementById("demo").innerhtml("This document is now under construction");
 }
 </script>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2&appId=151351795473882&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 </head>
 <body>
 <center><h1 class = "heading">Data base login panel</h1></center>
 <div class = "frame">
+<h3>Watch my youtube videos</h3>
+<a class = "button_watch" href = "Youtube/youtube.php" target = "_blank">Watch</a>
 <form method = "post" action = "log.php">
 <label><h3><center>Please log in</center></h3><br><br></label>
 <b>Input your user name: </b><br>
-<input type = "text" name = "user" class = "widthe" placeholder = "Enter your username"><br>
+<input type = "text" name = "user" class = "widthe" placeholder = "Enter your username" required><br>
 <b>Input your pass : </b> <br>
-<input type = "password" name = "pass" class = "widthe" placeholder = "Enter password"><br><br><br>
+<input type = "password" name = "pass" class = "widthe" placeholder = "Enter password" required><br><br><br>
 <b><input type = "submit" value = "Authorize" class = "width" color ="white"></b>
  <br>
  </form>
  </div>
 
+<div class="fb-comments" data-href="http://techtrickpedia.000webhostapp.com/" data-width="450" data-numposts="10"></div>
 <div class = "footer">
 	This website is created by Md.Masud karim. Copyright &copy from 2018-<?php echo date("Y");?>
 </div>
 <script>
 window.alert("Welcome to this website");
 </script>
+
+
 </body>
 </html>
